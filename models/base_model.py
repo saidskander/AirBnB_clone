7bf0922c81d3e770aff711a456bb2fbbd44bcbc5
saidskander <askander199h@outlook.com>
2020-07-01 20:11:37 +0100

    Done

--- models/base_model.py
+++ models/base_model.py
@@ -10,22 +10,24 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Init method """
-        if len(kwargs) is not 0:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = kwargs.get(key)
-                if key == "created_at":
-                    self.created_at = datetime.strptime(kwargs.get(key),
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                if key == "updated_at":
-                    self.updated_at = datetime.strptime(kwargs.get(key),
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                if key == "my_number":
-                    self.my_number = kwargs.get(key)
-                if key == "name":
-                    self.name = kwargs.get(key)
+        if kwargs:
+            if "created_at" not in kwargs.keys():
+                raise Exception("created_at not passed")
+            if "updated_at" not in kwargs.keys():
+                raise Exception("updated_at not passed")
+            if "id" not in kwargs.keys():
+                raise Exception("id not passed")
+            for x, y in kwargs.items():
+                if x == '__class__':
+                    pass
+                else:
+                    setattr(self, x, y)
+            self.created_at = datetime.datetime.strptime(
+                self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.datetime.strptime(
+                self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
