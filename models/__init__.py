94b74f553ac1b398cf02b71fb1097d865deae190
saidskander <askander199h@outlook.com>
2020-07-01 23:09:06 +0100

    Done

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+""" File models storage """
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """AIRBNB project Base module"""
-import uuid
-import datetime
+from datetime import datetime
+from uuid import uuid4
 import models
 
 
@@ -9,32 +9,30 @@ class BaseModel:
     """ BaseModel class rbnb project """
 
     def __init__(self, *args, **kwargs):
-        """ Init method """
-        if kwargs:
-            if "created_at" not in kwargs.keys():
-                raise Exception("created_at not passed")
-            if "updated_at" not in kwargs.keys():
-                raise Exception("updated_at not passed")
-            if "id" not in kwargs.keys():
-                raise Exception("id not passed")
-            for x, y in kwargs.items():
-                if x == '__class__':
-                    pass
-                else:
-                    setattr(self, x, y)
-            self.created_at = datetime.datetime.strptime(
-                self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.datetime.strptime(
-                self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+        """ Init method pass """
+        if len(kwargs) is not 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = kwargs.get(key)
+                if key == "created_at":
+                    self.created_at = datetime.strptime(kwargs.get(key),
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs.get(key),
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "my_number":
+                    self.my_number = kwargs.get(key)
+                if key == "name":
+                    self.name = kwargs.get(key)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def save(self):
         """update updated_at/datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
