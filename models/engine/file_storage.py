8a2045763fd953f64d090a3d2f969e532ee59662
saidskander <askander199h@outlook.com>
2020-07-01 17:54:28 +0100

    Done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,13 +3,6 @@
 import json
 import os.path
 from os import path
-from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.state import State
-from models.place import Place
-from models.review import Review
 
 
 class FileStorage():
