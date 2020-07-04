5855b303f7b862cd4c297491f0be3fb602127950
saidskander <askander199h@outlook.com>
2020-07-04 14:56:31 +0100

    Done

---
+++ console.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import cmd
+import shlex
+from models import storage
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import json
+
+my_class = {"BaseModel": BaseModel, "User": User, "State": State,
+            "City": City, "Amenity": Amenity, "Place": Place,
+            "Review": Review}
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Command hbnb Class """
+
+    prompt = '(hbnb) '
+    file = None
+
+    def do_EOF(self, arg):
+        'EOF command to exit the program'
+        return True
+
+    def do_quit(self, arg):
+        'Quit command to exit the program'
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
