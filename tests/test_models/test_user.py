6ffc33b8e1944073e3df0ed16334485a30c5eb00
saidskander <askander199h@outlook.com>
2020-07-04 18:02:10 +0100

    Done

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import unittest
-import pep8
 import os
+import pep8
+import unittest
 from models.user import User
 from models.engine.file_storage import FileStorage
 
@@ -24,19 +24,19 @@ class TestStringMethods(unittest.TestCase):
         pathfile2 = "tests/test_models/test_user.py"
         test = stylepep8.check_files([pathfile1, pathfile2])
         self.assertEqual(test.total_errors,
-                         0, "Found code Errors/warning).")
+                         0, "Found code Errors/warning")
 
 
-class TestModels(unittest.TestCase):
-    """ Funtion to test the BaseModel"""
+class Test_base_models(unittest.TestCase):
+    """setUp Funtion to test the BaseModel"""
 
     def setUp(self):
-        """ Set a variable """
-        self.user_1 = User()
-        self.user_1.name = 'Jeniffer'
-        self.user_1.lastname = "Vanegas"
-        self.user_1.email = 'airbnb@holbertonshool.com'
-        self.user_1.password = "root"
+        """ Seting variables """
+        self.user_first = User()
+        self.user_first.name = 'Jeniffer'
+        self.user_first.lastname = "Vanegas"
+        self.user_first.email = 'airbnb@holbertonshool.com'
+        self.user_first.password = "root"
         print("setUp")
 
     def tearDown(self):
@@ -59,40 +59,40 @@ class TestModels(unittest.TestCase):
 
     def test_place_city(self):
         """ check if the city name is create """
-        self.user_1.save()
+        self.user_first.save()
         self.assertTrue(os.path.isfile('file.json'))
-        self.assertTrue(hasattr(self.user_1, "__init__"))
-        self.assertTrue(hasattr(self.user_1, "email"))
-        self.assertTrue(hasattr(self.user_1, "password"))
-        self.assertTrue(hasattr(self.user_1, "first_name"))
-        self.assertTrue(hasattr(self.user_1, "last_name"))
+        self.assertTrue(hasattr(self.user_first, "__init__"))
+        self.assertTrue(hasattr(self.user_first, "email"))
+        self.assertTrue(hasattr(self.user_first, "password"))
+        self.assertTrue(hasattr(self.user_first, "first_name"))
+        self.assertTrue(hasattr(self.user_first, "last_name"))
 
     def test_user_name(self):
         """ check if the name is create """
-        self.assertEqual(self.user_1.name, 'Jeniffer')
+        self.assertEqual(self.user_first.name, 'Jeniffer')
 
     def test_user_lastname(self):
-        """ chaeck if the lastname is create """
-        self.assertEqual(self.user_1.lastname, "Vanegas")
+        """ check if the last name is create """
+        self.assertEqual(self.user_first.lastname, "Vanegas")
 
     def test_user_email(self):
-        """ chaeck if the email is create """
-        self.assertEqual(self.user_1.email, 'airbnb@holbertonshool.com')
+        """ check the email if its created """
+        self.assertEqual(self.user_first.email, 'airbnb@holbertonshool.com')
 
     def test_user_password(self):
-        """ chaeck if the password is create """
-        self.assertEqual(self.user_1.password, "root")
+        """ check the password if its created """
+        self.assertEqual(self.user_first.password, "root")
 
     def test_models_to_dict(self):
-        model_1 = self.user_1.to_dict()
-        self.assertIsInstance(model_1["created_at"], str)
-        self.assertIsInstance(model_1["updated_at"], str)
-        self.assertIsInstance(model_1["email"], str)
-        self.assertIsInstance(model_1["id"], str)
+        model_first = self.user_first.to_dict()
+        self.assertIsInstance(model_first["created_at"], str)
+        self.assertIsInstance(model_first["updated_at"], str)
+        self.assertIsInstance(model_first["email"], str)
+        self.assertIsInstance(model_first["id"], str)
 
     def test_user_instance(self):
-        """ check if user_1 is instance of User """
-        self.assertIsInstance(self.user_1, User)
+        """ check user_first if is instance of User """
+        self.assertIsInstance(self.user_first, User)
 
 if __name__ == '__main__':
     unittest.main()
