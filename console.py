be3c778d938f6112b4e799cfa7f9a9d3d2701601
saidskander <askander199h@outlook.com>
2020-07-04 16:39:29 +0100

    Done

--- console.py
+++ console.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
+"""r """
 import cmd
 import shlex
 from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import json
+
+my_class = {"BaseModel": BaseModel, "User": User, "State": State,
+            "City": City, "Amenity": Amenity, "Place": Place,
+            "Review": Review}
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Command hbnb Class """
+    """ Command HBNB Class """
 
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
     file = None
 
     def do_EOF(self, arg):
-        'EOF command to exit the program'
+        "EOF command to exit the program"
         return True
 
     def do_quit(self, arg):
-        'Quit command to exit the program\n'
+        "Quit command to exit the program"
         return True
 
     def emptyline(self):
